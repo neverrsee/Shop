a3392a39ba0d900cc11c8233f430c82452635e23
Eric <Megalovania4gg@gmail.com>
2024-07-07 17:10:47 +0300

    Добавление шаблонов и контекста из базы данных

--- Shop/views.py
+++ Shop/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
+from .models import Product, Order
+
 
 # Create your views here.
 
 def catalog(request):
-    return render(request, 'Shop/catalog.html')
+    products = Product.objects.all()
+    return render(request, 'Shop/catalog.html', {'products':products})
 
 def orders(request):
-    return render(request, 'Shop/orders.html')
+    orders = Order.objects.all()
+    return render(request, 'Shop/orders.html', {'orders':orders})
 
 def order_create(request):
     return render(request, 'Shop/order_create.html')
